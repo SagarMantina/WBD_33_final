8f6b2ce0851cb27192d039e440ae186587eae026
mantinasagar <sagarmantina11@gmail.com>
2025-05-01 15:44:37 +0530

    updated-dockerfile

--- solr/entrypoint.sh
+++ solr/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Start Solr in background
 solr start
-
-# Wait for Solr to fully start
 sleep 15
 
-# Create the core
 solr create -c mongodb_core
 
-# Post JSON data
-curl -X POST -H "Content-Type: application/json" --data-binary @/games_solr.json http://localhost:8983/solr/mongodb_core/update/json/docs?commit=true
+curl -X POST -H "Content-Type: application/json" \
+     --data-binary @/opt/solr/games_solr.json \
+     http://localhost:8983/solr/mongodb_core/update/json/docs?commit=true
 
-# Keep Solr running in foreground
 tail -f /dev/null
