802f3fd32f0c5510816c803a7b1f2fd0fd1dbc54
Mantina Sagar <123088264+SagarMantina@users.noreply.github.com>
2025-05-05 08:03:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -134,7 +134,7 @@ function readDataFromJSONFile(filename) {
 // Express routes for contacts and messages
 app.get("/contacts", async (req, res) => {
   try {
-    const currentUser = req.cookies.username;
+    const currentUser = header("x-username");
     if (!currentUser) {
       return res.status(401).json({ message: "Unauthorized" });
     }
@@ -157,7 +157,7 @@ app.get("/contacts", async (req, res) => {
 
 app.get("/messages/:recipient", async (req, res) => {
   try {
-    const currentUser = req.cookies.username;
+    const currentUser = header("x-username");
     const recipient = req.params.recipient;
 
     const messages = await Message.find({
@@ -173,6 +173,7 @@ app.get("/messages/:recipient", async (req, res) => {
     res.status(500).json({ message: "Error fetching messages" });
   }
 });
+
 
 // Socket.IO for real-time communication
 const server = http.createServer(app);
